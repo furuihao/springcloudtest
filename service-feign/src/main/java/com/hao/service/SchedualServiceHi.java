ed84c45284647f92f0af2f0a6388b457675aa04a
furuihao <94402146@qq.com>
2018-06-01 17:55:39 +0800

    1111111111111111111

--- service-feign/src/main/java/com/hao/service/SchedualServiceHi.java
+++ service-feign/src/main/java/com/hao/service/SchedualServiceHi.java
@@ -9,8 +9,8 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author hao
  * @create 2018/5/26
  * 定义一个feign接口，通过@ FeignClient（“服务名”），来指定调用哪个服务。
- * 比如在代码中调用了service-hi服务的“/hi”接口
- */
+        * 比如在代码中调用了service-hi服务的“/hi”接口
+        */
 @FeignClient(value = "service-hi")
 public interface SchedualServiceHi {
     @RequestMapping(value = "/hi",method = RequestMethod.GET)
