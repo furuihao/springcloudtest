4321c96bc4324cc1c046e114455957282f8a8f08
furuihao <94402146@qq.com>
2018-06-15 16:42:41 +0800

    高可用服务中心

--- eureka-client/src/main/java/com/hao/client/web/HelloController.java
+++ eureka-client/src/main/java/com/hao/client/web/HelloController.java
@@ -1,9 +1,14 @@
 package com.hao.client.web;
 
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * Created by gcb on 2018/5/16.
@@ -15,8 +20,13 @@ public class HelloController {
     @Value("${server.port}")
     private String port;
 
+    @Resource
+    private DiscoveryClient client ;
+
     @RequestMapping("/hi")
     public String home(@RequestParam String name) {
+        ServiceInstance instance = client.getLocalServiceInstance();
+        System.out.println("host："+instance.getHost()+",service_id:"+instance.getServiceId());
         return "hi " + name + ",i am from port:" + port;
     }
 
